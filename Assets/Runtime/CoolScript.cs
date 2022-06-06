d5419f7a1d47961bb278d78e7ad4715b26bab3ed
Roy van der Wijk <61933294+RoyvdWijk@users.noreply.github.com>
2022-06-06 11:06:17 +0200

    Update

--- Assets/Runtime/CoolScript.cs
+++ Assets/Runtime/CoolScript.cs
@@ -1,11 +1,14 @@
+using Microsoft.Extensions.Logging;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ILogger = Microsoft.Extensions.Logging.ILogger;
 
-public class CoolScript : MonoBehaviour
+public abstract class CoolScript : MonoBehaviour
 {
+    public abstract ILogger Logger { get; protected set; }
     public void Execute()
     {
-        Debug.Log("Cool script was executed.");
+        this.Logger.LogInformation("Cool script was executed.");
     }
 }
