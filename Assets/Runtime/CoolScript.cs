d86799fbb16046004b70c82fa691986160f6d0b8
Roy van der Wijk <61933294+RoyvdWijk@users.noreply.github.com>
2022-06-06 10:08:17 +0200

    Project commit

---
+++ Assets/Runtime/CoolScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoolScript : MonoBehaviour
+{
+    public void Execute()
+    {
+        Debug.Log("Cool script was executed.");
+    }
+}
